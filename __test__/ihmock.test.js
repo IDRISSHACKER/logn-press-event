1cf6fd6cbd07b2dafbd9937a9cbf473d65213094
IDRISSHACKER <idrisscoder@gmail.com>
2022-04-22 11:37:24 +0100

    Utilisation de la fonctionnalitée toEqual sur les objets

--- __test__/ihmock.test.js
+++ __test__/ihmock.test.js
@@ -2,9 +2,6 @@ const { ihmock } = require("../src/ihmock")
 
 test('calcule en apliicant la regle ih', () => {
     expect(ihmock(200)).toBe(600)
-    expect(ihmock(10000)).toBe(30000)
-    expect(ihmock(200000)).toBe(600000)
-    expect(ihmock(600000)).toBe(1800000)
 })
 
 
